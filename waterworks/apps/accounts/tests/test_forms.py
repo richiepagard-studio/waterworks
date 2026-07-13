0b4c4962bdf15b214b55f2d8831a7ebde16818d4
richie <richiepagard@gmail.com>
2026-07-13 13:32:25 -0400

    feat; Write a new testcase in accounts.
    - New test case in accounts for user profile update to test that the view redirects to the next URL after a successful profile updated.

--- waterworks/apps/accounts/tests/test_forms.py
+++ waterworks/apps/accounts/tests/test_forms.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 
 from apps.accounts.models import User
 from apps.accounts.forms import UserRegisterForm
@@ -64,3 +65,34 @@ class TestRegisterForm(TestCase):
 		self.assertEqual(len(form.errors), 1)
 		self.assertTrue(form.has_error)
 
+
+class TestUserProfileUpdateView(TestCase):
+	"""
+	Test cases for testing user profile update view.
+	"""
+	
+	def test_redirects_to_next_url_after_successful_update(self):
+		"""
+		Test that the view redirects to the next URL after a successful profile update.
+		"""
+
+		user = User.objects.create_user(
+			phone_number='+989128762334',
+			username='profiletestuser',
+			password='&,iN3Bys'
+		)
+		url = reverse('accounts:user-profile-update', kwargs={'user_id': user.id})
+		next_url = '/installations/1/'
+
+		response = self.client.post(
+			f'{url}?next={next_url}',
+			data={
+				'username': 'profiletestuser',
+				'first_name': 'Test',
+				'last_name': 'User',
+				'address': 'Tehran',
+			},
+		)
+
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response.url, next_url)
